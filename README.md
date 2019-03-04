0e15c3c79482a078d701a4a0031cbb0a69188c59
Matthew Michaelis <matthew.michaelis@ucdenver.edu>
2019-03-03 22:07:53 -0700

    Adding HW 2 requirements

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# week4
+# CSCI 3916
+
+Forked from https://github.com/shawnmccarthy/week4
+
--- server.js
+++ server.js
@@ -1,11 +1,16 @@
+// Base code from https://github.com/shawnmccarthy/week4
+
 var express = require('express');
 var http = require('http');
 var bodyParser = require('body-parser');
 var passport = require('passport');
-var authController = require('./auth');
 var authJwtController = require('./auth_jwt');
 db = require('./db')(); //global hack
 var jwt = require('jsonwebtoken');
+
+var hardcoded_user = "matt";
+var hardcoded_pass = "1234";
+
 
 var app = express();
 app.use(bodyParser.json());
@@ -32,64 +37,134 @@ function getJSONObject(req) {
     return json;
 }
 
-router.route('/post')
-    .post(authController.isAuthenticated, function (req, res) {
-            console.log(req.body);
-            res = res.status(200);
-            if (req.get('Content-Type')) {
-                console.log("Content-Type: " + req.get('Content-Type'));
-                res = res.type(req.get('Content-Type'));
+
+// Signup
+router.route('/signup')
+    .post( function(req, res) {
+        if (!req.body.username || !req.body.password) {
+            res.json({success: false, msg: 'Please pass username and password.'});
+        } else {
+            if(db.findOne(req.body.username)){
+                res.json({success: false, msg: 'User already exists.'});
+            }else{
+                var newUser = {
+                    username: req.body.username,
+                    password: req.body.password
+                };
+                db.save(newUser); //no duplicate checking
+                res.json({success: true, msg: 'Successful created new user.'});
+
             }
-            var o = getJSONObject(req);
-            res.json(o);
         }
-    );
+    })
+    
+    .get(function(req, res) {
+        res.status(501).send({success: false, msg: 'Unsupported access type for signup.'});
+    })
 
-router.route('/postjwt')
-    .post(authJwtController.isAuthenticated, function (req, res) {
-            console.log(req.body);
-            res = res.status(200);
-            if (req.get('Content-Type')) {
-                console.log("Content-Type: " + req.get('Content-Type'));
-                res = res.type(req.get('Content-Type'));
-            }
-            res.send(req.body);
-        }
-    );
+    .put(function(req, res) {
+        res.status(501).send({success: false, msg: 'Unsupported access type for signup.'});
+    })
 
-router.post('/signup', function(req, res) {
-    if (!req.body.username || !req.body.password) {
-        res.json({success: false, msg: 'Please pass username and password.'});
-    } else {
-        var newUser = {
-            username: req.body.username,
-            password: req.body.password
-        };
-        // save the user
-        db.save(newUser); //no duplicate checking
-        res.json({success: true, msg: 'Successful created new user.'});
-    }
-});
+    .delete(function(req, res) {
+        res.status(501).send({success: false, msg: 'Unsupported access type for signup.'});
+    })
 
-router.post('/signin', function(req, res) {
+    .patch(function(req, res) {
+        res.status(501).send({success: false, msg: 'Unsupported access type for signup.'});
+    });
 
+
+
+// Sign-in
+router.route('/signin')
+
+    .post(function(req, res) {
         var user = db.findOne(req.body.username);
 
         if (!user) {
             res.status(401).send({success: false, msg: 'Authentication failed. User not found.'});
-        }
-        else {
+        }else {
             // check if password matches
-            if (req.body.password == user.password)  {
+            if (req.body.password === user.password)  {
                 var userToken = { id : user.id, username: user.username };
-                var token = jwt.sign(userToken, process.env.SECRET_KEY);
+                var token = jwt.sign(userToken, process.env.UNIQUE_KEY);
                 res.json({success: true, token: 'JWT ' + token});
             }
             else {
                 res.status(401).send({success: false, msg: 'Authentication failed. Wrong password.'});
             }
-        };
-});
+        }
+    })
+    .get(function(req, res) {
+        res.status(501).send({success: false, msg: 'Unsupported access type for sign in.'});
+    })
+
+    .put(function(req, res) {
+        res.status(501).send({success: false, msg: 'Unsupported access type for sign in.'});
+    })
+
+    .delete(function(req, res) {
+        res.status(501).send({success: false, msg: 'Unsupported access type for sign in.'});
+    })
+
+    .patch(function(req, res) {
+        res.status(501).send({success: false, msg: 'Unsupported access type for sign in.'});
+    });
+
+
+
+// Movies
+router.route('/movies')
+    .post(function (req, res) {
+        q = "None";
+        h = req.headers;
+
+        if(req.body.query){
+            q = req.body.query;
+        }
+        res.status(200).send({ status:200,msg: 'POST movies',headers:h,query:q,env:process.env.UNIQUE_KEY});
+    })
+
+    .get(function (req, res) {
+         q = "None";
+         h = req.headers;
+
+         if(req.body.query){
+             q = req.body.query;
+         }
+         res.status(200).send({ status:200,msg: 'GET movies',headers:h,query:q,env:process.env.UNIQUE_KEY});
+    })
+
+    .put(authJwtController.isAuthenticated, function (req, res) {
+        q = "None";
+        h = req.headers;
+
+        if(req.body.query){
+            q = req.body.query;
+        }
+        res.status(200).send({ status:200,msg: 'PUT movies',headers:h,query:q,env:process.env.UNIQUE_KEY});
+    })
+
+    .delete(function (req, res) {
+        if(req.body.username === hardcoded_user && req.body.password === hardcoded_pass){
+            q = "None";
+            h = req.headers;
+            if(req.body.query){
+                q = req.body.query;
+            }
+            res.status(200).send({ status:200,msg: 'DELETE movies',headers:h,query:q,env:process.env.UNIQUE_KEY});
+        }else{
+            res.status(401).send({ status:401,msg: 'Unauthorized'});
+        }
+
+
+    })
+
+    .patch(function (req, res) {
+        res.status(501).send({success: false, msg: 'Unsupported access type for movies.'});
+    });
+
 
 app.use('/', router);
 app.listen(process.env.PORT || 8080);
